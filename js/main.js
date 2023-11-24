7169c3c62212a62fed2f7df85c68732a9a385118
Dilara-E <dogan.dilara@elev.ga.ntig.se>
2023-11-24 13:29:17 +0100

    basen

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function addYear() {
     var yearList = document.getElementById('yearList');
     var year = yearList.childElementCount; // Nästa år
-
+  
     var listItem = document.createElement('li');
     listItem.className = 'item';
     listItem.innerHTML = `
       <h1>År ${year}</h1>
-      <span class="title">Räkning</span>
+      <span class="title">Beräkning</span>
       <div class="container">
         <span class="undertitle">Eget kapital</span>
         <input type="number" class="equity" placeholder="Ange eget kapital">
@@ -20,44 +20,46 @@ function addYear() {
         <input type="number" class="liabilities" placeholder="Ange skulder">
       </div>
     `;
-
+  
     yearList.appendChild(listItem);
   }
-
+  
   function calculate() {
     var yearList = document.getElementById('yearList').children;
     var resultContainer = document.getElementById('result');
     var totalBalances = [];
     var totalEquity = 0;
-
+  
     for (var i = 0; i < yearList.length; i++) {
       var equityInput = yearList[i].querySelector('.equity');
       var assetsInput = yearList[i].querySelector('.assets');
       var liabilitiesInput = yearList[i].querySelector('.liabilities');
-
+  
       var equity = parseFloat(equityInput.value) || 0;
       var assets = parseFloat(assetsInput.value) || 0;
       var liabilities = parseFloat(liabilitiesInput.value) || 0;
-
+  
       if (i === 0) {
         totalEquity = equity;
       }
-
+  
       var balance = assets - liabilities;
       totalBalances.push(balance);
-
+  
       // Resultatberäkning
-      var result = totalEquity + balance;
-      totalEquity = result; // Uppdatera eget kapital för nästa år
+      if (i > 0) {
+        totalEquity += balance; // Påverkas av föregående års balans
+      }
     }
-
+  
     // Visa resultatet för varje år
     resultContainer.innerHTML = '';
     for (var i = 0; i < totalBalances.length; i++) {
       resultContainer.innerHTML += `Balans år ${i}: ${totalBalances[i].toFixed(2)} | Resultat år ${i}: ${totalEquity.toFixed(2)}<br>`;
     }
   }
-
+  
   // Lägg till två år automatiskt
   addYear();
   addYear();
+  
